55e788eaae158a0652e668f43ae664a980849418
Michał Chrząszcz <mchrzaszcz90@gmail.com>
2024-10-18 16:36:14 +0200

    Add pre-post install scripts

--- preinstall.js
+++ preinstall.js
@@ -1,6 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
-
+require('dotenv').config();
 
 if(!process.env.FLOTIQ_API_KEY) {
     throw new Error('Missing FLOTIQ_API_KEY environment variable');
